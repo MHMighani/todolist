e117445fcc407a64b717d5cdd653e1d5a6f37b76
MHMighani <mh.mgh2012@gmail.com>
2022-07-12 18:43:40 +0430

    Feat: delete todo list

--- src/components/todoLists.jsx
+++ src/components/todoLists.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
+import { TodoListsContext } from "../contexts/listsContext";
 import TodoList from "./todoList";
 
-function TodoLists({ todoLists }) {
+  const { todoLists, deleteTodoList } = useContext(TodoListsContext);
+
   return (
     <ul>
       {todoLists.map((todoList) => (
         <li className="todoList" key={todoList.id}>
           <TodoList todoList={todoList} />
+            <button onClick={() => deleteTodoList(todoList.id)}>Delete</button>
         </li>
       ))}
     </ul>
